c7ab2267632463651a008803f853b22561b723f8
Takeshi Ota <bamboo@Bamboo-mac.local>
2020-11-04 12:10:57 +0900

    子要素の確認

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 $(function(){
-  $('.box1').mouseover(function (){
-    $('.box1').css({'background-color': '#0000FF'});
-  });
-  $('.box1').mouseout(function (){
-    $(".box1").css({'background-color': '#FF0000'});
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
